d43732dfec69c99f4031128eae53f380df4418b9
umang299 <umang91999@gmail.com>
2023-12-17 20:08:11 +0530

    fix: linting errors

--- src/client.py
+++ src/client.py
@@ -1,18 +1,31 @@
 """
-ChromaDB Client - A module for interacting with ChromaDB for collection management and data upload.
+ChromaDB Client - A module for interacting with ChromaDB for
+collection management and data upload.
 
-This module provides a client class for interacting with ChromaDB, allowing the creation, querying, and deletion of collections.
+
+This module provides a client class for interacting with ChromaDB, allowing
+the creation, querying, and deletion of collections.
 It also provides methods for loading and uploading data to the database.
 
 Args:
     openai_api_key (str): The API key for accessing the OpenAI service.
     encoding (str, optional): The text encoding to use (default is 'utf-8').
-    required_ext (list, optional): A list of required file extensions (default is ['.pdf']).
-    num_files_limit (int, optional): The maximum number of files to load (default is 2).
-    chunk_size (int, optional): The chunk size for text splitting (default is 512).
-    chunk_overlap (int, optional): The chunk overlap for text splitting (default is 25).
-    host (str, optional): The hostname or IP address of the ChromaDB server (default is 'localhost').
-    port (int, optional): The port number for the ChromaDB server (default is 8000).
+    required_ext (list, optional): A list of required file extensions
+                                   (default is ['.pdf']).
+
+    num_files_limit (int, optional): The maximum number of files to load
+                                     (default is 2).
+
+    chunk_size (int, optional): The chunk size for text splitting
+                                (default is 512).
+
+    chunk_overlap (int, optional): The chunk overlap for text splitting
+                                   (default is 25).
+
+    host (str, optional): The hostname or IP address of the ChromaDB server
+                          (default is 'localhost').
+    port (int, optional): The port number for the ChromaDB server
+                          (default is 8000).
 
 Attributes:
     openai_api_key (str): The API key for accessing the OpenAI service.
@@ -23,13 +36,17 @@ Attributes:
     num_files_limit (int): The maximum number of files to load.
     chunk_size (int): The chunk size for text splitting.
     chunk_overlap (int): The chunk overlap for text splitting.
-    client (chromadb.HttpClient): An instance of the ChromaDB HTTP client for communication with ChromaDB.
+    client (chromadb.HttpClient): An instance of the ChromaDB HTTP client for
+                                  communication with ChromaDB.
 
 Methods:
     get_collection(collection_name): Get an existing collection by name.
-    create_collection(collection_name): Create a new collection or get an existing one by name.
+    create_collection(collection_name): Create a new collection or get an
+                                        existing one by name.
     load_data(file_path): Load data from a file into ChromaDB.
-    get_info(collection_name, n=10): Get information about a collection, including the count of items and a preview of the first 'n' items.
+    get_info(collection_name, n=10): Get information about a collection,
+                                     including the count of items and a
+                                     preview of the first 'n' items.
 
 Example Usage:
     client = ChromaDBClient(openai_api_key='your_openai_api_key')
@@ -51,23 +68,38 @@ sys.path.append(cwd)
 
 class ChromaDBClient:
     """
-    A client class for interacting with ChromaDB, responsible for creating, querying, and deleting collections.
+    A client class for interacting with ChromaDB, responsible for creating,
+    querying, and deleting collections.
 
     Args:
         openai_api_key (str): The API key for accessing the OpenAI service.
-        encoding (str, optional): The text encoding to use (default is 'utf-8').
-        required_ext (list, optional): A list of required file extensions (default is ['.pdf']).
-        num_files_limit (int, optional): The maximum number of files to load (default is 2).
-        chunk_size (int, optional): The chunk size for text splitting (default is 512).
-        chunk_overlap (int, optional): The chunk overlap for text splitting (default is 25).
-        host (str, optional): The hostname or IP address of the ChromaDB server (default is 'localhost').
-        port (int, optional): The port number for the ChromaDB server (default is 8000).
+        encoding (str, optional): The text encoding to use (default is 'utf-8')
+        required_ext (list, optional): A list of required file extensions
+                                       (default is ['.pdf']).
+
+        num_files_limit (int, optional): The maximum number of files to load
+                                         (default is 2).
+
+        chunk_size (int, optional): The chunk size for text splitting
+                                    (default is 512).
+
+        chunk_overlap (int, optional): The chunk overlap for text splitting
+                                       (default is 25).
+
+        host (str, optional): The hostname or IP address of the ChromaDB
+                              server (default is 'localhost').
+
+        port (int, optional): The port number for the ChromaDB server
+                              (default is 8000).
 
     Methods:
         get_collection(collection_name): Get an existing collection by name.
-        create_collection(collection_name): Create a new collection or get an existing one by name.
+        create_collection(collection_name): Create a new collection or get an
+                                            existing one by name.
         load_data(file_path): Load data from a file into ChromaDB.
-        get_info(collection_name, n=10): Get information about a collection, including the count of items and a preview of the first 'n' items.
+        get_info(collection_name, n=10): Get information about a collection,
+                                         including the count of items and a
+                                         preview of the first 'n' items.
 
     Example Usage:
         client = ChromaDBClient(openai_api_key='your_openai_api_key')
@@ -76,26 +108,32 @@ class ChromaDBClient:
     """
 
     def __init__(self, openai_api_key: str = None,
-                 encoding: str = None, 
+                 encoding: str = None,
                  required_ext: list = None,
                  num_files_limit: int = None,
-                 chunk_size: int = None, 
+                 chunk_size: int = None,
                  chunk_overlap: int = None,
-                 host: str = 'localhost', 
-                 port: int = 8000
-            ):
+                 host: str = 'localhost',
+                 port: int = 8000):
         """
         Initialize the ChromaDBClient.
 
         Args:
             openai_api_key (str): The API key for accessing the OpenAI service.
-            encoding (str, optional): The text encoding to use (default is 'utf-8').
-            required_ext (list, optional): A list of required file extensions (default is ['.pdf']).
-            num_files_limit (int, optional): The maximum number of files to load (default is 2).
-            chunk_size (int, optional): The chunk size for text splitting (default is 512).
-            chunk_overlap (int, optional): The chunk overlap for text splitting (default is 25).
-            host (str, optional): The hostname or IP address of the ChromaDB server (default is 'localhost').
-            port (int, optional): The port number for the ChromaDB server (default is 8000).
+            encoding (str, optional): The text encoding to use
+                                      (default is 'utf-8').
+            required_ext (list, optional): A list of required file extensions
+                                           (default is ['.pdf']).
+            num_files_limit (int, optional): The maximum number of files to
+                                             load (default is 2).
+            chunk_size (int, optional): The chunk size for text splitting
+                                        (default is 512).
+            chunk_overlap (int, optional): The chunk overlap for text
+                                           splitting (default is 25).
+            host (str, optional): The hostname or IP address of the ChromaDB
+                                  server (default is 'localhost').
+            port (int, optional): The port number for the ChromaDB server
+                                  (default is 8000).
 
         Returns:
             None
@@ -105,9 +143,14 @@ class ChromaDBClient:
         self.port = port
         self.text_encoding = 'utf-8' if encoding == '' else encoding
         self.required_exts = ['.pdf'] if required_ext is [] else required_ext
-        self.num_files_limit = 2 if num_files_limit is None else num_files_limit 
+
+        if num_files_limit is None:
+            self.num_files_limit = 2
+        else:
+            self.num_files_limit = num_files_limit
+
         self.chunk_size = 512 if chunk_size is None else chunk_size
-        self.chunk_overlap = 25 if chunk_overlap is None else chunk_overlap 
+        self.chunk_overlap = 25 if chunk_overlap is None else chunk_overlap
         self.client = self.__initialize_client()
 
     def __initialize_client(self):
@@ -128,7 +171,8 @@ class ChromaDBClient:
         Create and return an OpenAI text embedding model.
 
         Returns:
-            embedding_functions.OpenAIEmbeddingFunction: An instance of the OpenAI text embedding model.
+            embedding_functions.OpenAIEmbeddingFunction: An instance of the
+            OpenAI text embedding model.
         """
         model = embedding_functions.OpenAIEmbeddingFunction(
             api_key=self.openai_api_key,
@@ -157,17 +201,19 @@ class ChromaDBClient:
         Create a new collection or get an existing one by name.
 
         Args:
-            collection_name (str): The name of the collection to create or retrieve.
+            collection_name (str): The name of the collection to create or
+            retrieve.
 
         Returns:
-            chromadb.Collection: An instance of the created or retrieved collection.
+            chromadb.Collection: An instance of the created or retrieved
+            collection.
         """
         collection = self.client.create_collection(
             name=collection_name,
             embedding_function=self.__embedding_model()
         )
         return collection
-    
+
     def __dataloader(self, file_path):
         loader = SimpleDirectoryReader(
                         input_files=[file_path],
@@ -175,28 +221,29 @@ class ChromaDBClient:
                         required_exts=self.required_exts,
                         num_files_limit=self.num_files_limit
                     )
-        
+
         return loader
-    
+
     def __node_splitter(self):
         splitter = TokenTextSplitter(
                         chunk_size=self.chunk_size,
                         chunk_overlap=self.chunk_overlap
                     )
         return splitter
-    
+
     def load_data(self, file_path):
         """
         Load data from a file into ChromaDB.
 
         Args:
-            file_path (str): The path to the data file to be uploaded to the database.
+            file_path (str): The path to the data file to be uploaded to the
+            database.
 
         Returns:
             list: A list of data nodes loaded into the database.
         """
         loader = self.__dataloader(file_path=file_path)
-        splitter  = self.__node_splitter()
+        splitter = self.__node_splitter()
 
         documents = loader.load_data()
         nodes = splitter.get_nodes_from_documents(documents=documents)
@@ -204,14 +251,16 @@ class ChromaDBClient:
 
     def get_info(self, collection_name, n=10):
         """
-        Get information about a collection, including the count of items and a preview of the first 'n' items.
+        Get information about a collection, including the count of items and a
+        preview of the first 'n' items.
 
         Args:
             collection_name (str): The name of the collection to query.
             n (int): The number of items to preview (default is 10).
 
         Returns:
-            dict: A dictionary containing collection information, including 'count' and 'items'.
+            dict: A dictionary containing collection information, including
+            'count' and 'items'.
         """
         collection = self.client.get_collection(
             name=collection_name,
@@ -221,4 +270,4 @@ class ChromaDBClient:
         return {
             'count': collection.count(),
             'items': collection.peek(limit=n)
-        }
+            }
