7598e7127dfa485600f2e878d8cf0a2d3ab63a19
umang299 <umang91999@gmail.com>
2023-12-17 20:29:15 +0530

    fix: linting issues

--- test/client.py
+++ test/client.py
@@ -1,15 +1,12 @@
 import os
-import sys
 import unittest
 from unittest.mock import MagicMock
-
-cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(cwd)
-
 from src.client import ChromaDBClient
 from src.utils import load_yaml_file
 
-conf = load_yaml_file(filename=os.path.join(cwd, 'config.yaml'))
+conf = load_yaml_file(filename=os.path.join(os.path.dirname(__file__),
+                                            '..',
+                                            'config.yaml'))
 
 
 class TestChromaDBClient(unittest.TestCase):
@@ -20,16 +17,19 @@ class TestChromaDBClient(unittest.TestCase):
         self.assertIsNotNone(self.client)
 
     def test_get_collection(self):
-        self.client.client.get_or_create_collection = MagicMock(return_value=conf['collection_name'])
+        self.client.client.get_or_create_collection = MagicMock(
+                        return_value=conf['collection_name']
+                        )
         collection = self.client.get_collection(conf['collection_name'])
         self.assertEqual(collection, conf['collection_name'])
 
     def test_create_collection(self):
-        self.client.client.create_collection = MagicMock(return_value=conf['new_collection_name'])
+        self.client.client.create_collection = MagicMock(
+                        return_value=conf['new_collection_name']
+                        )
         collection = self.client.create_collection(conf['new_collection_name'])
         self.assertEqual(collection, conf['new_collection_name'])
 
 
 if __name__ == '__main__':
     unittest.main()
-    
