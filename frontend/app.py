d437398e43e85ee6365ee2bb39bb86312218d6cc
umang299 <umang91999@gmail.com>
2023-12-18 13:07:23 +0530

    fix: linting issues

--- frontend/app.py
+++ frontend/app.py
@@ -1,21 +1,22 @@
 import os
-import sys
 import streamlit as st
-
-cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(cwd)
-
-os.makedirs(name=os.path.join(cwd, 'data'), exist_ok=True)
-os.makedirs(name=os.path.join(cwd, 'conversation'), exist_ok=True)
 
 from src.client import ChromaDBClient
 from src.utils import save_uploaded_file, is_api_key_valid, load_yaml_file
 from main import get_response, upload
 
+
+cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+os.makedirs(name=os.path.join(cwd, 'data'), exist_ok=True)
+os.makedirs(name=os.path.join(cwd, 'conversation'), exist_ok=True)
+
 api_status = is_api_key_valid()
 st.write(f'API Key Status : {api_status}')
 
-api_key = load_yaml_file(filename='config.yaml')['OPENAI_API_KEY']
+api_key = load_yaml_file(
+    filename=os.path.join(cwd, 'config.yaml')
+    )['OPENAI_API_KEY']
+
 client = ChromaDBClient(openai_api_key=api_key)
 
 st.title("Document GPT")
