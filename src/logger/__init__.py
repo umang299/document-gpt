136411dcf334fc322a5f0fb4aa8c31c06e06474a
umang299 <umang91999@gmail.com>
2023-12-19 14:48:41 +0530

    add: logger modules

---
+++ src/logger/__init__.py
@@ -0,0 +1,6 @@
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s: %(levelname)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
