17251d03bdf7815c23839b96594560ca87261d0a
umang299 <umang91999@gmail.com>
2023-12-21 10:43:08 +0530

    mod: read path to logger directory from config file.

--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,18 @@
 import os
 import sys
 import logging
+from utils import load_yaml_file
 
 cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(cwd)
 
+config = load_yaml_file(filename=os.path.join(cwd, 'config.yaml'))[0]
+
 # Configure logging
 logging.basicConfig(
-                    filename=os.path.join(cwd, 'logs', 'client.log'),
+                    filename=os.path.join(
+                        cwd, config['logs_dir'], 'client.log'
+                        ),
                     level=logging.DEBUG,
                     format='%(asctime)s: %(levelname)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
