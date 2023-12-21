10cd5c8fac9d72dd9d6e95682b8072eb4c36e203
umang299 <umang91999@gmail.com>
2023-12-21 10:14:33 +0530

    removed logger module from src

--- src/logger/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-# Configure logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s: %(levelname)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
