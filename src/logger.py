776d8e778b42ecb84b7e3d0c5c22a9fcc4abf4cc
umang299 <umang91999@gmail.com>
2023-12-21 23:50:18 +0530

    modified logic

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,32 @@
 import os
-import sys
+import yaml
 import logging
-from .utils import load_yaml_file
+
+
+def load_yaml_file(filename):
+    """
+    Load data from a YAML file.
+
+    Args:
+    filename (str): The path to the YAML file.
+
+    Returns:
+    dict: The data loaded from the YAML file.
+    """
+    try:
+        with open(filename, 'r') as file:
+            data = yaml.safe_load(file)
+
+        return data
+
+    except Exception as e:
+        logging.error(f'Load yaml file: {e}')
+        return None
+
 
 cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(cwd)
 
-config = load_yaml_file(filename=os.path.join(cwd, 'config.yaml'))[0]
+config = load_yaml_file(filename=os.path.join(cwd, 'config.yaml'))
 os.makedirs(name=os.path.join(cwd, config['logs_dir']), exist_ok=True)
 
 # Configure logging
