5144353031949ccf75a6542918d1cc0e3ecc6619
umang299 <umang91999@gmail.com>
2023-12-21 11:06:46 +0530

    mod: moodified logic to check if client logger directory is created.

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import logging
-from utils import load_yaml_file
+from .utils import load_yaml_file
 
 cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(cwd)
 
 config = load_yaml_file(filename=os.path.join(cwd, 'config.yaml'))[0]
+os.makedirs(name=os.path.join(cwd, config['logs_dir']), exist_ok=True)
 
 # Configure logging
 logging.basicConfig(
