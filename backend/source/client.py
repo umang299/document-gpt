2cc2caa466c15610e38314b7c5429a85e8a82b78
umang299 <umang91999@gmail.com>
2024-01-04 02:09:03 +0530

    updated chromadb client.

--- backend/source/client.py
+++ backend/source/client.py
@@ -1,203 +1,131 @@
 import os
+from uuid import uuid4
+
 import chromadb
 from chromadb.utils import embedding_functions
 
-import llama_index
 from llama_index import SimpleDirectoryReader
 from llama_index.node_parser import TokenTextSplitter
+from llama_index.vector_stores import ChromaVectorStore
+from llama_index import VectorStoreIndex, ServiceContext
 
-from .log import logging
-from .config import CONFIG_PATH
-from .utils import load_yaml_file
+from .config import DB_DIR, DATA_DIR, CONFIG_PATH
+from .utils import load_yaml_file, load_conversation, conversation_logger
+
+client = chromadb.PersistentClient(path=DB_DIR)
+openai_api_key = os.environ['OPENAI_API_KEY']
 
 
-class ChromaDBClient:
-    def __init__(self,
-                 encoding: str = None,
-                 required_ext: list = None,
-                 num_files_limit: int = None,
-                 ):
+def embedding_model():
+    model = embedding_functions.OpenAIEmbeddingFunction(
+        api_key=openai_api_key,
+        model_name='text-embedding-ada-002'
+    )
+    return model
 
-        self.config = load_yaml_file(filename=CONFIG_PATH)
-        self.openai_api_key = os.getenv('OPENAI_API_KEY')
-        self.host = os.getenv('HOST')
-        self.port = os.getenv('PORT')
 
-        self.text_encoding = 'utf-8' if encoding == '' else encoding
-        self.required_exts = ['.pdf'] if required_ext is [] else required_ext
+def list_collections():
+    collections = client.list_collections()
+    return [col.name for col in collections]
 
-        if num_files_limit is None:
-            self.num_files_limit = 2
-        else:
-            self.num_files_limit = num_files_limit
 
-        self.client = self.__initialize_client()
+def get_collection(collection_name):
+    try:
+        collection = client.get_collection(
+                            name=collection_name,
+                            embedding_function=embedding_model()
+                        )
+        return collection
+    except ValueError as e:
+        print(e)
+        return False
 
-    def __initialize_client(self):
-        try:
-            client = chromadb.HttpClient(
-                host=self.host,
-                port=self.port
+
+def create_collection(collection_name):
+    list_collection = list_collections()
+    if collection_name not in list_collection:
+        collection = client.create_collection(
+                        name=collection_name,
+                        embedding_function=embedding_model()
+                    )
+    else:
+        collection = client.get_collection(
+                        name=collection_name
+                    )
+    return collection
+
+
+def delete_collection(collection_name):
+    client.delete_collection(name=collection_name)
+
+
+def upload(collection_name, text):
+    file_path = os.path.join(DATA_DIR, f'{uuid4()}.txt')
+
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(text)
+
+    collection = client.get_collection(
+                    name=collection_name,
+                    embedding_function=embedding_model()
+                )
+
+    documents = SimpleDirectoryReader(
+                    input_files=[file_path],
+                ).load_data()
+
+    splitter = TokenTextSplitter(
+                    chunk_size=512,
+                    chunk_overlap=25,
+                    separator='.',
+                    include_metadata=True
+                )
+
+    nodes = splitter.get_nodes_from_documents(
+                    documents=documents
+                )
+    for node in nodes:
+        hash = node.hash
+        content = node.text
+
+        if content != '':
+            content_metadata = {
+                'id': hash,
+                'Page_Text': content
+            }
+
+            collection.add(
+                documents=[content],
+                metadatas=[content_metadata],
+                ids=[str(id)]
             )
-            return client
 
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
+    return True
 
-    def __embedding_model(self):
-        try:
-            model = embedding_functions.OpenAIEmbeddingFunction(
-                api_key=self.openai_api_key,
-                model_name="text-embedding-ada-002"
-            )
-            return model
 
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
-
-    def get_collection(self, collection_name):
-        assert type(collection_name) is str, f"""TypeError: Expected
-        collection name as string, received {type(collection_name)}"""
-
-        try:
-            collection = self.client.get_collection(
-                name=collection_name,
-                embedding_function=self.__embedding_model()
-            )
-
-            assert type(collection) is \
-                chromadb.api.models.Collection.Collection, """
-            Received invalid collection, check chromadb documentation.
-            """
-
-            assert collection.name == collection_name, f"""
-            Returned collection name {collection.name} does not match the input
-            collection name {collection_name}
-            """
-
-            return collection
-
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
-
-    def create_collection(self, collection_name):
-        try:
-            collection = self.client.create_collection(
-                name=collection_name,
-                embedding_function=self.__embedding_model()
-            )
-
-            assert type(collection) is \
-                chromadb.api.models.Collection.Collection, """
-                Received invalid collection, check chromadb documentation.
-                """
-
-            assert collection.name == collection_name, f"""
-            Created collection name {collection.name} does not match the input
-            collection name {collection_name}
-            """
-            return collection
-
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
-
-    def __dataloader(self, file_path):
-        try:
-            assert type(file_path) is str, f"""TypeError: Expected
-            file_path as string, received {type(file_path)}"""
-
-            loader = SimpleDirectoryReader(
-                            input_files=[file_path],
-                            encoding=self.text_encoding,
-                            required_exts=self.required_exts,
-                            num_files_limit=self.num_files_limit
+def response(collection_name: str, message: str):
+    collection = get_collection(
+                            collection_name=collection_name,
                         )
 
-            assert type(loader) is \
-                llama_index.readers.file.base.SimpleDirectoryReader, """
-            Received invalid dataloader, check llama-index documentation.
-            """
-
-            return loader
-
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
-
-    def __node_splitter(self):
-        try:
-            splitter = TokenTextSplitter(
-                            chunk_size=self.config['CHUNK_SIZE'],
-                            chunk_overlap=self.config['CHUNK_OVERLAP']
+    config = load_yaml_file(filename=CONFIG_PATH)
+    chat_history = load_conversation()
+    vector_store = ChromaVectorStore(chroma_collection=collection)
+    service_context = ServiceContext.from_defaults(
+                            chunk_size=config['CHUNK_SIZE'],
+                            chunk_overlap=config['CHUNK_OVERLAP']
                         )
 
-            return splitter
+    index = VectorStoreIndex.from_vector_store(
+                            vector_store,
+                            service_context=service_context,
+                        )
 
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
-
-    def load(self, file_path):
-        try:
-            loader = self.__dataloader(file_path=file_path)
-            splitter = self.__node_splitter()
-
-            documents = loader.load_data()
-            nodes = splitter.get_nodes_from_documents(documents=documents)
-
-            return nodes
-
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
-
-    def get_info(self, collection_name, n=10):
-        try:
-            assert type(n) is int, f"""TypeError: Expected
-            n as Integer, received {type(n)}"""
-
-            collection = self.client.get_collection(
-                name=collection_name,
-                embedding_function=self.__embedding_model()
-            )
-
-            assert type(collection) is \
-                chromadb.api.models.Collection.Collection, """
-                Received invalid collection, check chromadb documentation.
-                """
-
-            return {
-                'count': collection.count(),
-                'items': collection.peek(limit=n),
-                }
-
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
-
-    def get_all_collections(self):
-        try:
-            client = self.__initialize_client()
-            collections = client.list_collections()
-            collections = [c.name for c in collections]
-            logging.info('Fetched list of collections in the database')
-            return {'collections': collections}
-
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
-
-    def delete_collection(self, collection_name):
-        try:
-            client = self.__initialize_client()
-            client.delete_collection(name=collection_name)
-
-            logging.info(msg=f'Deleted {collection_name} collections')
-
-        except Exception as e:
-            logging.error(msg=f'Error: {e}')
-            return None
+    chat_engine = index.as_chat_engine()
+    conversation_logger(message=message, role='user')
+    response = chat_engine.chat(
+                            message=message,
+                            chat_history=chat_history
+                        )
+    conversation_logger(message=response.response, role='assistant')
+    return response.response
