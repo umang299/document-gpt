cf39578066ed8f7a3aa8e3b2df7d8ba14be50743
umang299 <umang91999@gmail.com>
2024-01-04 02:09:26 +0530

    add: config file

--- backend/source/config.py
+++ backend/source/config.py
@@ -4,7 +4,10 @@ import sys
 cwd = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(cwd)
 
+os.makedirs(os.path.join(cwd, 'data'), exist_ok=True)
+
 CONFIG_PATH = os.path.join(cwd, 'config.yaml')
 CONV_DIR = os.path.join(cwd, 'conversations')
 DB_DIR = os.path.join(cwd, 'storage')
+DATA_DIR = os.path.join(cwd, 'data')
 CWD = cwd
